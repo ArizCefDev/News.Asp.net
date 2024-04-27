4b95d37b886f87900d3062a804f35b29328b0720
Ariz Ceferli <106917027+ArizCefDev@users.noreply.github.com>
2024-04-27 16:28:38 +0400

    Add project files.

---
+++ Business/Concrete/PostService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Business.Abstract;
+using DataAccess.Entity;
+using DataAccess.MyContext;
+using DTO.EntityDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PostService : BaseSevice<PostDTO, Post, PostDTO>, IPost
+    {
+        public PostService(IMapper mapper, AppDbContext context) : base(mapper, context)
+        {
+        }
+    }
+}
---
+++ News.Asp.net/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using News.Asp.net.Models;
+using System.Diagnostics;
+
+namespace News.Asp.net.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPost _post;
+
+		public HomeController(IPost post)
+		{
+			_post = post;
+		}
+
+		public IActionResult Index()
+        {
+            var values = _post.GetAll();
+            return View(values.OrderByDescending(row=>row.Id).Take(4));
+        }
+
+       
+    }
+}
